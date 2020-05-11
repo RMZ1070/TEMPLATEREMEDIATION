38021ee8b79fa2aef41d4906ec17a1840295b4e9
Rmz <xfa1070@hotmail.com>
2020-05-11 15:56:12 +0200

    TemplateRemediation

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts/master')
+@section('content')
+
+@include('/partials/navbar')
+@include('/components/header')
+@include('/partials/about')
+@include('/components/services')
+@include('/components/contact')
+@include('/components/footer')
+
+
+@endsection
